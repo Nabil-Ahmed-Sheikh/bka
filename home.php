dbc4fe13920fb11f2c402a1926954ea78f9cf5c4
Nabil Ahmed <nabil.ahmed@northsouth.edu>
2020-02-22 12:03:24 +0600

    initialize the frontend

---
+++ home.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>বাংলাদেশ খেলাফত আন্দোলন</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/site.css">
+	<link href="assets/fontawesome/css/all.css" rel="stylesheet">
+	<script type="text/javascript" src=""></script>
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0"></script>	 
+	<header class="header">
+		<div class="container">
+			<div class="preheader row">
+				<div class="col-lg-7">
+				</div>
+				<div class="col-lg-5">
+					<ul class="social">
+						<li> <a href="https://www.facebook.com/bka.org/" target="_blank"> <i class="fab fa-facebook"></i> </a> </li>
+						<li> <a href="https://twitter.com/BkaOrg"> <i class="fab fa-twitter" target="_blank"></i> </a> </li>
+						<li> <a href="https://www.linkedin.com/in/bangladesh-khelafat-3442a1190/" target="_blank"> <i class="fab fa-linkedin" target="_blank"></i> </a> </li>
+						<li> <a href="tel:+88 017 112 63452">Cell.: +88 017 112 63452</a> </li>
+						<li> <a href="join.php">সদস্য হোন</a> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>		
+	<section class="menu-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<h5 class="eq-padding">
+						<img src="assets/images/logo.png"/>
+					</h5>
+				</div>
+				<div class="col-lg-9">
+					<nav class="navbar navbar-expand-lg navbar-light">
+					  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					    <span class="navbar-toggler-icon"></span>
+					  </button>
+					  <div class="collapse navbar-collapse align-items-center" id="navbarResponsive">
+					    <ul class="navbar-nav ml-auto align-items-center">
+					      <li class="nav-item active">
+					        <a class="nav-link" href="/">মূল পাতা <span class="sr-only">(current)</span></a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="introduction.php">সংগঠন পরিচিতি</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">সংবাদ ও প্রেস বিজ্ঞপ্তি</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">প্রবন্ধ-নিবন্ধ</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">ওডিও-ভিডিও</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">প্রকাশনা</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">কেন্দ্রীয় কমিটি</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">আমীরে শরীয়ত</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">যোগাযোগ</a>
+					      </li>
+					      <!-- <li class="nav-item dropdown">
+					        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					          Dropdown link
+					        </a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					          <a class="dropdown-item" href="#">Action</a>
+					          <a class="dropdown-item" href="#">Another action</a>
+					          <a class="dropdown-item" href="#">Something else here</a>
+					        </div>
+					      </li> -->
+					    </ul>
+					  </div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="slider">
+		<!-- <img src="assets/images/introduction-2.jpg" class="img-responsive">	 -->	
+	</section>
+
+	<section class="content-with-sidebar padding-ten brand-color">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<h3> ফিচার প্রবন্ধ </h3>
+					<!-- <ul>
+						<li> -->
+							<div>
+								<h3>খেলাফত পদ্ধতির সরকার দেশ ও জাতির জন্য কি করবে </h3>
+								<div>
+									<p>আল্লাহ তা‘আলার সার্বভৌমত্বের ঘোষণা দেবে এবং কুরআন ও সুন্নাহর বিধানের পরিপূর্ণ বাস্তবায়ন করবে, তথা খেলাফত পদ্ধতির শাসন ব্যবস্থা চালু করবে। </p>
+
+									<p>বিশ্ববাসীর নিকট মহান আল্লাহ তা‘আলার হুকুম-আহকাম যথাযথভাবে পৌঁছাবে। </p>
+
+									<p>রাষ্ট্রের মুসলিম-অমুসলিম সকল নাগরিকের জান-মাল, ইজ্জত, আবরু সংরক্ষণের নিশ্চয়তা বিধান করবে। সকল নাগরিকের মৌলিক চাহিদা তথা-খাদ্য, বস্ত্র, বাসস্থান, শিক্ষা, চিকিৎসা ও নিরাপত্তা নিশ্চিত করবে।</p>
+
+									<p>সকল নাগরিকের অধিকার সংরক্ষণকল্পে ইনসাফ ভিত্তিক বিচার ব্যবস্থা দ্রুত বাস্তবায়ন করবে।</p>
+
+									<p>নারীর সম্ভ্রম ও শালীনতা রক্ষা এবং যথাযথ নারী অধিকার প্রদানের নিশ্চয়তা দেবে। </p>
+
+									<p>মহান আল্লাহ ও প্রিয় রাসুল সাল্লাল্লাহু আলাইহি ওয়াসাল্লাম এর বিধান মুতাবিক পরোপুরি পর্দার হেফাজত করে মহিলাদের সামাজিক কর্মকান্ডে অংশ গ্রহনের সুযোগ দেবে। </p>
+
+									<p>ইসলাম বিদ্বেষীদের সকল ষড়যন্ত্র নস্যাৎকল্পে মুসলিম জনসাধারণের অন্তরে দাওয়াত ও জিহাদের অনুপ্রেরণা  যোগাবে। </p>
+
+									<p>খেলাফত পদ্ধতির শাসন ব্যবস্থা প্রতিষ্ঠা করা বর্তমান সময়ের সকল সমস্যার একমাত্র সমাধান। তাই খেলাফত পদ্ধতির শাসন প্রতিষ্ঠার মহান লক্ষ্যে হযরত হাফেজ্জী হুজুর (রহ:) কর্তৃক প্রতিষ্ঠিত বাংলাদেশ খেলাফত আন্দোলনে যোগ দিন। </p>
+								</div>
+							</div>
+							<div>
+
+							</div>
+						<!-- </li>
+
+					</ul> -->
+				</div>
+				<div class="col-md-3">
+					<h3>সাম্প্রতিক ইভেন্ট</h3>
+					<h5> বিক্ষোভ মিছিল ও সমাবেশ </h5>
+					<img src="assets/images/kasmir.jpg" class="img-responsive"/>
+					<p> কাশ্মীরে মুসলিম গণহত্যা, নির্যাতন বন্ধ, ভারতীয় সৈন্য প্রত্যাহার ও কাশ্মীরের স্বাধীনতার দাবীতে বাংলাদেশ খোলাফত আন্দোলনের উদ্যোগে বিক্ষোভ মিছিল ও সমাবেশ</p>
+					<p>তারিখ ও সময়: ৩১ আগস্ট, শনিবার; বিকাল ৪.০০ টা </p>
+					<p>স্থান: বায়তুল মোকাররম উত্তর গেট, রাজধানী ঢাকা </p>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="content-without-sidebar padding-ten">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>				
+			</div>
+		</div>
+	</section>
+	
+	<section class="content-without-sidebar liflet padding-ten announce">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="text-center">
+						<p>হযরত হাফেজ্জী হুজুর রহ. প্রতিষ্ঠিত</p>
+						<h4>বাংলাদেশ খেলাফত আন্দোলন</h4>
+						<h5> ওয়েবসাইট শীঘ্রই নতুন রুপে পরিবর্ধিত আকারে আসছে ইনশাআল্লাহ। </h5> <p> বাংলাদেশ খেলাফত আন্দোলনের কার্যক্রম ও সর্বশেষ খবরাখবর জানতে সংগঠনের <a href="https://www.facebook.com/bka.org/" target="_blank">ফেসবুক পেজে</a> লাইক দিয়ে সক্রিয় থাকুন এবং <a href="https://twitter.com/BkaOrg" target="_blank">টুইটার প্রোফাইল</a> ফলো করুন। </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="content-without-sidebar liflet padding-ten announce">
+		<!-- <div class="container">
+			<div class="row">
+				<div class="col-md-12"> -->
+					<!-- <img src="assets/images/introduction-1.jpg" class="img-responsive"> -->
+				<!-- </div>
+			</div>
+		</div> -->
+	</section>
+
+	<section class="content-with-sidebar category-wise-news padding-ten">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer class="footer padding-ten brand-color">
+		<div class="container">
+			<div class="row main-footer">
+				<div class="col-md-4">
+					<!-- <ul class="footer-links">
+						<li> <a href="#"> ইতিহাস, পরিচিতি ও আদর্শ </a> </li>
+						<li> <a href="#"> লক্ষ্য, উদ্দেশ্য ও কর্মসূচি </a> </li>
+						<li> <a href="#"> কর্মপদ্ধতি </a> </li>
+						<li> <a href="#"> নীতি নির্ধারণী ঘোষণা </a> </li>
+						
+					</ul>
+					<hr/> -->
+					<h5>বাংলাদেশ খেলাফত আন্দোলন</h5>
+					<hr/>
+					<address>
+						কেন্দ্রীয় কার্যালয়: দারুল খেলাফত, ৩১৪/২ কেল্লার মোড়, লালবাগ, ঢাকা-১২১১ বাংলাদেশ। <br/> 
+						ফোন: ০২-৯৬৩০১০২, ০১৭৮৩৩৩৯৪৭৪ <br/>
+						ইমেইল: khelafatandolon01@gmail.com 
+					</address>
+				</div>
+				<div class="col-md-4">
+					<div class="fb-page" data-href="https://www.facebook.com/bka.org/"  data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/bka.org/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/bka.org/">Bangladesh Khelafat Andolon</a></blockquote></div>
+				</div>
+				<div class="col-md-4">
+					<a class="twitter-timeline" data-height="214" href="https://twitter.com/BkaOrg?ref_src=twsrc%5Etfw">Tweets by Bangladesh Khelafat Andolon</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<div class="footer-credit">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 center">
+					<p class="text-center">&copy; All Rights Reserved. Designed and Developed By <a href="http://softrithm.com" target="_blank">SoftRithm IT Limited</a> </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ introduction.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>বাংলাদেশ খেলাফত আন্দোলন</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/site.css">
+	<link href="assets/fontawesome/css/all.css" rel="stylesheet">
+	<script type="text/javascript" src=""></script>
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0"></script>	 
+	<header class="header">
+		<div class="container">
+			<div class="preheader row">
+				<div class="col-lg-7">
+				</div>
+				<div class="col-lg-5">
+					<ul class="social">
+						<li> <a href="https://www.facebook.com/bka.org/" target="_blank"> <i class="fab fa-facebook"></i> </a> </li>
+						<li> <a href="https://twitter.com/BkaOrg"> <i class="fab fa-twitter" target="_blank"></i> </a> </li>
+						<li> <a href="https://www.linkedin.com/in/bangladesh-khelafat-3442a1190/" target="_blank"> <i class="fab fa-linkedin" target="_blank"></i> </a> </li>
+						<li> <a href="tel:+88 017 112 63452">Cell.: +88 017 112 63452</a> </li>
+						<li> <a href="join.php">সদস্য হোন</a> </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>		
+	<section class="menu-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<h5 class="eq-padding">
+						<img src="assets/images/logo.png"/>
+					</h5>
+				</div>
+				<div class="col-lg-9">
+					<nav class="navbar navbar-expand-lg navbar-light">
+					  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					    <span class="navbar-toggler-icon"></span>
+					  </button>
+					  <div class="collapse navbar-collapse align-items-center" id="navbarResponsive">
+					    <ul class="navbar-nav ml-auto align-items-center">
+					      <li class="nav-item active">
+					        <a class="nav-link" href="/">মূল পাতা <span class="sr-only">(current)</span></a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="introduction.php">সংগঠন পরিচিতি</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">সংবাদ ও প্রেস বিজ্ঞপ্তি</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">প্রবন্ধ-নিবন্ধ</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">ওডিও-ভিডিও</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">প্রকাশনা</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">কেন্দ্রীয় কমিটি</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">আমীরে শরীয়ত</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#">যোগাযোগ</a>
+					      </li>
+					      <!-- <li class="nav-item dropdown">
+					        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					          Dropdown link
+					        </a>
+					        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					          <a class="dropdown-item" href="#">Action</a>
+					          <a class="dropdown-item" href="#">Another action</a>
+					          <a class="dropdown-item" href="#">Something else here</a>
+					        </div>
+					      </li> -->
+					    </ul>
+					  </div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="slider">
+		<img src="assets/images/introduction-2.jpg" class="img-responsive">		
+	</section>
+
+
+
+	
+	<br/> <br/> <br/>
+
+	<section class="content-without-sidebar liflet padding-ten announce">
+		<!-- <div class="container">
+			<div class="row">
+				<div class="col-md-12"> -->
+					<img src="assets/images/introduction-1.jpg" class="img-responsive">
+				<!-- </div>
+			</div>
+		</div> -->
+	</section>
+
+
+
+	<footer class="footer padding-ten brand-color">
+		<div class="container">
+			<div class="row main-footer">
+				<div class="col-md-4">
+					<!-- <ul class="footer-links">
+						<li> <a href="#"> ইতিহাস, পরিচিতি ও আদর্শ </a> </li>
+						<li> <a href="#"> লক্ষ্য, উদ্দেশ্য ও কর্মসূচি </a> </li>
+						<li> <a href="#"> কর্মপদ্ধতি </a> </li>
+						<li> <a href="#"> নীতি নির্ধারণী ঘোষণা </a> </li>
+						
+					</ul>
+					<hr/> -->
+					<h5>বাংলাদেশ খেলাফত আন্দোলন</h5>
+					<hr/>
+					<address>
+						কেন্দ্রীয় কার্যালয়: দারুল খেলাফত, ৩১৪/২ কেল্লার মোড়, লালবাগ, ঢাকা-১২১১ বাংলাদেশ। <br/> 
+						ফোন: ০২-৯৬৩০১০২, ০১৭৮৩৩৩৯৪৭৪ <br/>
+						ইমেইল: khelafatandolon01@gmail.com 
+					</address>
+				</div>
+				<div class="col-md-4">
+					<div class="fb-page" data-href="https://www.facebook.com/bka.org/"  data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/bka.org/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/bka.org/">Bangladesh Khelafat Andolon</a></blockquote></div>
+				</div>
+				<div class="col-md-4">
+					<a class="twitter-timeline" data-height="214" href="https://twitter.com/BkaOrg?ref_src=twsrc%5Etfw">Tweets by Bangladesh Khelafat Andolon</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<div class="footer-credit">
+		<div class="container">
+			<div class="row copyright">
+				<div class="col-md-12 center">
+					<p class="text-center">&copy; All Rights Reserved. Designed and Developed By <a href="http://softrithm.com" target="_blank">SoftRithm IT Limited</a> </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
